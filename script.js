88463e216e0833562c79bfbf6e114a9a5439c3dc
Arif Momin <arifmominweb@gmail.com>
2025-02-18 21:32:03 +0600

    cfgh

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import {sisterQuotes} from "./Quots.js";
+import {friendshipQuotes} from "./Quots.js";
 
 const quotes = document.querySelector('.quotes');
 const author = document.querySelector('.author');
 const button = document.querySelector('button');
 function quotsRun () {
-    const randomNumber = Math.floor(Math.random()* sisterQuotes.length );
-    quotes.textContent = sisterQuotes[randomNumber].quote;
-    author.textContent = sisterQuotes[randomNumber].author;
+    const randomNumber = Math.floor(Math.random()* friendshipQuotes.length );
+    quotes.textContent = friendshipQuotes[randomNumber].quote;
+    author.textContent = friendshipQuotes[randomNumber].author;
 }
 quotsRun();
 button.addEventListener('click', (quotsRun))
